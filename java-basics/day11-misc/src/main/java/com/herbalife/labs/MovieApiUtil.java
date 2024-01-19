21e9207e15f0ce6d51abeaa9ec1002fda7c206a6
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-01-19 07:39:18 +0530

    Day11

--- java-basics/day11-misc/src/main/java/com/herbalife/labs/MovieApiUtil.java
+++ java-basics/day11-misc/src/main/java/com/herbalife/labs/MovieApiUtil.java
@@ -14,6 +14,7 @@ public class MovieApiUtil {
     public String fetchMovieDetails(String movieName) throws IOException, InterruptedException {
         movieName = URLEncoder.encode(movieName);
         String url = OMDB_API_BASE_URL.formatted(movieName);
+        System.out.println(url);
         HttpRequest request = HttpRequest
                 .newBuilder()
                 .uri(java.net.URI.create(url))
