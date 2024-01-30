2abd54e3a3d2db7d7de15a95a88bd1e47ce12a30
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-01-30 09:28:25 +0530

    Day15

--- quarkus-rest-db/src/main/java/com/herbalife/examples/orm/PersonService.java
+++ quarkus-rest-db/src/main/java/com/herbalife/examples/orm/PersonService.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class PersonService {
@@ -19,5 +20,15 @@ public class PersonService {
     public List<Person> getPersons() {
         return personRepository
                 .listAll();
+    }
+
+    @Transactional
+    public void updateAge(int id, int age) {
+        Optional<Person> optionalPerson = personRepository.findByIdOptional(id);
+        if (optionalPerson.isPresent()) {
+            Person person = optionalPerson.get();
+            person.setAge(age);
+            personRepository.persist(person);
+        }
     }
 }
