3eeced07dd0a9b884f2ac5b55bd949628df7e518
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-02-13 09:15:49 +0530

    Day22

--- day22-k8s-api/src/main/java/com/herbalife/SimpleResource.java
+++ day22-k8s-api/src/main/java/com/herbalife/SimpleResource.java
@@ -3,9 +3,21 @@ package com.herbalife;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/")
 public class SimpleResource {
+    @ConfigProperty(name = "welcome.message")
+    String welcomeMessage;
+
+    @ConfigProperty(name = "bye.message")
+    String byeMessage;
+
+    @GET
+    @Path("greetings")
+    public String greetings() {
+        return welcomeMessage + ". " + byeMessage;
+    }
 
     @GET
     @Path("hello/{name}")
